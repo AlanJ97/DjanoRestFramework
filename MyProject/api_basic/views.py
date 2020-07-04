098cb4a1100b14974fe7050586f80bb1543c51d9
Alan <alannavaseg_02@hotmail.com>
2020-07-03 19:39:55 -0500

    CReando la primera vista

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .serializers import ArticleSerializer
 
 # Create your views here.
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many = True)
+        return JsonResponse(serializer.data, safe = False)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 201)
+        return JsonResponse(serializer.errors, status = 400)
