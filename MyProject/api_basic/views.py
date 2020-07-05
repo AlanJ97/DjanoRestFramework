41ad92a815282a755fc836549d3b52cdcfb8f1a8
Alan <alannavaseg_02@hotmail.com>
2020-07-05 14:15:20 -0500

    AGregando la basic authentication

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, generics, mixins
 from rest_framework.views import APIView
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
@@ -20,7 +20,8 @@ class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.Crea
 
     lookup_field = 'id'
 
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+   # authentication_classes = [SessionAuthentication, BasicAuthentication]
+   authentication_classes  = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request, id = None):
