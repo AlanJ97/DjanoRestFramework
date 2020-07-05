529da30c4e875b21abc62cef4698bddac401827f
Alan <alannavaseg_02@hotmail.com>
2020-07-04 21:07:10 -0500

    GEneric view creada

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -6,10 +6,32 @@ from .models import Article
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, mixins
 from rest_framework.views import APIView
+
 # Create your views here.
 
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin,
+        mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    lookup_field = 'id'
+
+    def get(self, request, id = None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+    
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id = None):
+        return  self.update(request)
+    
+    def delete(self, request, id):
+        return self.destroy(request, id)
 class ArticleAPIView(APIView):
     def get(self, request):
         articles = Article.objects.all()
